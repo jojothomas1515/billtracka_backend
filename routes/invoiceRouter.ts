af96f0eea2661aa1772fb8cb74a03f6b041257f8
Jojo Thomas <jojothomas1515@gmail.com>
2023-11-25 21:47:46 +0100

    working

--- routes/invoiceRouter.ts
+++ routes/invoiceRouter.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   createInvoice,
   getInvoicesByOwner,
+  updateInvoice,
 } from '../controllers/invoiceController.js';
 
 const router: Router = Router();
 
 router.post('/', createInvoice);
 router.get('/', getInvoicesByOwner);
+router.post('/:id(\\w+)', updateInvoice);
 
 export default router;
--- types/types.d.ts
+++ types/types.d.ts
@@ -1,15 +1,10 @@
-import { Response } from 'express';
-import { IncomingHttpHeaders } from 'http';
 import { JwtPayload } from 'jsonwebtoken';
 import User from '../models/userModel.js';
-
-export type RTResponse = Promise<Response | undefined>;
 
 declare global {
   namespace Express {
     interface Request {
       user: User;
-      headers: IncomingHttpHeaders & { token: string };
     }
   }
 }
