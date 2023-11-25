d8d275c4f6ea48b892e563cffce470cfe720d7ca
Jojo Thomas <jojothomas1515@gmail.com>
2023-11-25 23:13:04 +0100

    service

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 import defaultRouter from './routes/index.js';
-// import fs from 'fs';
 import { errorHandler } from './middleware/errorHandler.js';
+import cors from 'cors';
 import { Express } from 'express';
 
 const app: Express = express();
 
-// const accessLog = fs.createWriteStream('/tmp/access.log', {
-//     flags: 'a',
-// });
-
 const logger = morgan('dev');
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
