b1fbf0f0004b63323333eefc0bfdcf4358db8bac
Jojo Thomas <jojothomas1515@gmail.com>
2023-11-27 20:29:33 +0100

    updated

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -4,9 +4,11 @@ import {
   deleteUser,
   changePassword,
 } from '../controllers/userController.js';
+import { isAuth } from '../middleware/isAuthenticated.js';
 
 const router: Router = Router();
 
+router.use(isAuth);
 router.put('/', updateUser);
 router.delete('/:id([\\w-]+)', deleteUser);
 router.put('/change-password', changePassword);
